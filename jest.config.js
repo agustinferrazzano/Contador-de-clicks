62f7649a1179838c2987ad2ecf85d949367ff729
Ferrazzano <agustin.ferrazzano123@gmail.com>
2025-06-06 15:40:25 -0300

    chore: remove jest.config.js file as it is no longer needed

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// Configuración de Jest para un proyecto que utiliza Babel y React
-export default {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',  // Habilitar transformaciones para los archivos JS/JSX
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-  testEnvironment: 'jsdom',
-};
