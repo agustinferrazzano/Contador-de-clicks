d59127d30329aff7e382e8ed451bba82c3151df0
Ferrazzano <agustin.ferrazzano123@gmail.com>
2025-06-08 20:12:24 -0300

    refactor: update incrementarClicks.test.js to use incrementarClick and improve test structure

--- tests/jest/incrementarClicks.test.js
+++ tests/jest/incrementarClicks.test.js
@@ -1,26 +1,26 @@
-import { inicializarContador } from '../../src/main.js';
+import { incrementarClick, configurarBotonClick } from "../../src/main.js"
 
-describe('Test contador', () => {
-  let boton;
+describe('Pruebas para la función incrementarClick', () => {
   let contador;
+  let boton;
 
   beforeEach(() => {
-    document.body.innerHTML = `
+    
+    document.body.innerHTML = ` 
       <button id="botonClick">Click me</button>
       <p id="contador">Clicks: 0</p>
     `;
-
     boton = document.getElementById('botonClick');
     contador = document.getElementById('contador');
 
-    inicializarContador(boton, contador);
+
+    configurarBotonClick(boton,contador);
+  }); 
+
+  test('Debería incrementar el contador correctamente', () => {
+    expect(incrementarClick()).toBe(1);  
+    expect(incrementarClick()).toBe(2); 
+    expect(incrementarClick()).toBe(3);  
   });
 
-  test('Incrementa correctamente', () => {
-    boton.click();
-    expect(contador.textContent).toBe('Clicks: 1');
-
-    boton.click();
-    expect(contador.textContent).toBe('Clicks: 2');
-  });
 });
