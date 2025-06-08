61b3d39e77777205aaaac4e43a96009a205105a9
Ferrazzano <agustin.ferrazzano123@gmail.com>
2025-06-08 20:52:38 -0300

    fix: restore support file path in cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 
 /*module.exports =*/ export default defineConfig({
   e2e: {
-    supportFile: false, 
+    supportFile: "test/cypress/support/e2e.js",
     specPattern: "test/cypress/e2e/**/*.{js,jsx,ts,tsx,cy.js}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
