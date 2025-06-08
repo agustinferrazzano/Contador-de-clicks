69e0be1a7751e2b4c281c05dd09375968f568a62
Ferrazzano <agustin.ferrazzano123@gmail.com>
2025-06-08 20:18:19 -0300

    refactor: restructure main.js to improve click handling and localStorage integration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-const boton = document.getElementById('botonClick');
-const contador = document.getElementById('contador');
+let clicks = 0;
 
-let clicks = parseInt(localStorage.getItem('clicks')) || 0;
-contador.textContent = `Clicks: ${clicks}`;
-
-boton.addEventListener('click', () => {
+export function incrementarClick() {
   clicks++;
-  localStorage.setItem('clicks', clicks);
-  contador.textContent = `Clicks: ${clicks}`;
+  return clicks;  
+}
+
+export function configurarBotonClick(boton,contador) {
+  boton.addEventListener('click', () => {
+    incrementarClick();
+    contador.textContent = 'Clicks: ' + clicks;
+  });
+}
+
+const elementocontador = document.getElementById('contador');
+const elementoboton = document.getElementById('botonClick');
+
+document.addEventListener('DOMContentLoaded', () => { //para asegurar que primero se cargue el dom, y despues llamar a la funcion que escucha los eventos
+  configurarBotonClick(elementoboton,elementocontador);
 });
