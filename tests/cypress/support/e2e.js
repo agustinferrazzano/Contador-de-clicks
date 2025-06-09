478486688476c76be9cb6d8999b84569e7745dac
Ferrazzano <agustin.ferrazzano123@gmail.com>
2025-06-08 22:02:56 -0300

    fix: update e2e.js comments and change import syntax; restore example.json fixture

--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -1,8 +1,17 @@
-// cypress/support/e2e.js
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
 
-// Este archivo se carga antes de tus tests y puedes agregar aquí configuraciones globales
-
-// Ejemplo: importar comandos personalizados
-require('./commands');
-
-// Si no tienes comandos personalizados, podés dejarlo vacío.
+// Import commands.js using ES2015 syntax:
+import './commands';
