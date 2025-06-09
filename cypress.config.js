cb6929552ce51a0fe9a240c63632f0bca35b0cc0
Ferrazzano <agustin.ferrazzano123@gmail.com>
2025-06-08 22:15:15 -0300

    fix: correct specPattern path in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "cypress";
 /*module.exports =*/ export default defineConfig({
   e2e: {
     supportFile: 'tests/cypress/support/e2e.js', // ruta corregida
-    specPattern: "test/cypress/e2e/**/*.{js,jsx,ts,tsx,cy.js}",
+    specPattern: "tests/cypress/e2e/**/*.{js,jsx,ts,tsx,cy.js}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
