9a26e17d5c63a23ec230b676bf84b932d777ab1f
AgustinF <agustin.ferrazzano123@gmail.com>
2025-06-12 11:33:09 -0300

    Configuracion del test E2E

--- tests/cypress/e2e/contadorclicks.cy.js
+++ tests/cypress/e2e/contadorclicks.cy.js
@@ -1,6 +1,6 @@
 describe('Contador de Clicks', () => {
   beforeEach(() => { //antes de cada it, visita la pagina
-    cy.visit('https://continuous-integration-rbne.onrender.com');
+    cy.visit(' https://contador-de-clicks.onrender.com');
   });
 
   it('debe incrementar el contador al hacer click', () => { 
